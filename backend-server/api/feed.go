72798509d8fde5361cbd867aecb4c1108f27e389
simon <ljx1680535@163.com>
2024-06-13 11:04:13 +0800

    rss parse

--- backend-server/api/feed.go
+++ backend-server/api/feed.go
@@ -6,6 +6,7 @@ import (
 	"bytetrade.io/web3os/backend-server/common"
 	"bytetrade.io/web3os/backend-server/http/response/json"
 	"bytetrade.io/web3os/backend-server/model"
+	"bytetrade.io/web3os/backend-server/service"
 	"go.uber.org/zap"
 
 	"bytetrade.io/web3os/backend-server/http/request"
@@ -46,4 +47,15 @@ func (h *handler) knowledgeRefreshFeed(w http.ResponseWriter, r *http.Request) {
 
 	common.Logger.Info("knowledge refresh feed", zap.String("feedID", feedID))
 	json.NoContent(w, r)
+}
+
+func (h *handler) rssParse(w http.ResponseWriter, r *http.Request) {
+
+	url := request.QueryStringParam(r, "url", "")
+	feed := service.RssParseFromURL(url)
+	if feed == nil {
+		json.OK(w, r, model.ParseFeedResponseModel{Code: -1})
+	} else {
+		json.OK(w, r, model.ParseFeedResponseModel{Code: 0, Data: model.GetFeedParseModel(feed)})
+	}
 }
