4adda35763376003d4b4927667cfd897bedc0c86
simon <ljx1680535@163.com>
2024-09-04 09:05:04 +0800

    remove cookie required

--- backend-server/http/client/cookie.go
+++ backend-server/http/client/cookie.go
@@ -56,7 +56,7 @@ func LoadCookieInfo(host string) []model.SettingDomainRespModel {
 	common.Logger.Info("start load cookie info ", zap.String("host", host))
 	request, _ := http.NewRequest("POST", settingUrl, bytes.NewBuffer(reqJsonByte))
 	request.Header.Set("Content-Type", "application/json")
-	//request.Header.Set("Cookie", "auth_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjY0ODg5MjQsImlhdCI6MTcyNTI3OTMyNCwiaXNzIjoia3ViZXNwaGVyZSIsInN1YiI6Im1tY2hvbmcyMDIxIiwidG9rZW5fdHlwZSI6ImFjY2Vzc190b2tlbiIsInVzZXJuYW1lIjoibW1jaG9uZzIwMjEiLCJleHRyYSI6eyJ1bmluaXRpYWxpemVkIjpbInRydWUiXX19.ZB4eMgKE_UYR0-iX7UBHaa_etsE4vUGbzZlUs_BsHGQ; authelia_session=!60*K-bP%SFXg4YGs4b#m6moBUQj3-Z7")
+	//request.Header.Set("Cookie", "auth_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjY1Nzk0OTEsImlhdCI6MTcyNTM2OTg5MSwiaXNzIjoia3ViZXNwaGVyZSIsInN1YiI6Im1tY2hvbmcyMDIxIiwidG9rZW5fdHlwZSI6ImFjY2Vzc190b2tlbiIsInVzZXJuYW1lIjoibW1jaG9uZzIwMjEiLCJleHRyYSI6eyJ1bmluaXRpYWxpemVkIjpbInRydWUiXX19.SsN5bZKoZ_eKBo5_I0FlaJcSBXMvbVQjiTPyb6sHq4A; authelia_session=nO1TW3lVuUUQI^_DA1Ez_btJvc5!NPgt")
 
 	client := &http.Client{Timeout: time.Second * 5}
 	response, err := client.Do(request)
