9a956ff0f64b7097c4212ed8af54e0d194a17cdc
simon <ljx1680535@163.com>
2025-06-01 16:05:03 +0800

    dev

--- backend-server/api/k8s.go
+++ backend-server/api/k8s.go
@@ -10,15 +10,8 @@ import (
 	"bytetrade.io/web3os/backend-server/http/request"
 )
 
-func (h *handler) getPvcAnnotation2(w http.ResponseWriter, r *http.Request) {
-	bflUser := request.RouteStringParam(r, "bfl_user")
-	common.Logger.Error("get pvc annotation", zap.String("bfl_user", bflUser))
-	annotation, _ := common.GetPvcAnnotation(bflUser)
-
-	json.OK(w, r, annotation)
-}
 func (h *handler) getPvcAnnotation(w http.ResponseWriter, r *http.Request) {
-	bflUser := request.RouteStringParam(r, "bfl_user")
+	bflUser := request.QueryStringParam(r, "bfl_user", "")
 	common.Logger.Error("get pvc annotation", zap.String("bfl_user", bflUser))
 	annotation, _ := common.GetPvcAnnotation(bflUser)
 
