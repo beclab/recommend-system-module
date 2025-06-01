fda65e8b9f7125932e8caf36379708ac8d4d11d0
simon <ljx1680535@163.com>
2025-06-01 12:57:38 +0800

    add log

--- backend-server/api/k8s.go
+++ backend-server/api/k8s.go
@@ -5,13 +5,14 @@ import (
 
 	"bytetrade.io/web3os/backend-server/common"
 	"bytetrade.io/web3os/backend-server/http/response/json"
+	"go.uber.org/zap"
 
 	"bytetrade.io/web3os/backend-server/http/request"
 )
 
 func (h *handler) getPvcAnnotation(w http.ResponseWriter, r *http.Request) {
 	bflUser := request.RouteStringParam(r, "bfl_user")
-
+	common.Logger.Error("get pvc annotation", zap.String("bfl_user", bflUser))
 	annotation, _ := common.GetPvcAnnotation(bflUser)
 
 	json.OK(w, r, annotation)
